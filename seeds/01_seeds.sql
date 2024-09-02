f87eb470e2e91611a6771031b69c1a3af88e1d06
xtremeyou <xtremeyou@hotmail.com>
2024-09-02 13:15:51 -0400

    add changes to table data so the data is more i-line with other tables

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -6,8 +6,8 @@ VALUES ('Maverick', 'xtremeyou@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED
 INSERT INTO properties (owner_id, title, description, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, thumbnail_photo_url, cover_photo_url, active, country, street, city, province, post_code)
 VALUES ( 
   1, 
-  'BEst place EVA', 
-  'humble and fancy!', 
+  'Speed lamp', 
+  'description', 
   369, 2, 2, 3, 
   'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 
   'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 
@@ -16,45 +16,45 @@ VALUES (
   '435 Days Rd', 
   'Kingston', 
   'Ontario', 
-  'kyk_35t'
+  'kyk 35t'
   ),
   ( 
   1, 
-  'BEst place EVA', 
-  'humble and fancy!', 
+  'Blank corner', 
+  'description', 
   369, 2, 2, 3, 
   'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 
   TRUE, 
   'Canada', 
-  '435 Days Rd', 
+  '893 Acres Dr', 
   'Kingston', 
   'Ontario', 
-  'kyk_35t'
+  'K7K 99tO'
   ),
   ( 
   2, 
-  'BEst place EVA', 
-  'humble and fancy!', 
+  'Habit mix', 
+  'description', 
   369, 2, 2, 3, 
   'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 
   TRUE, 
   'Canada', 
-  '435 Days Rd', 
+  '239 Dinamo Rd', 
   'Kingston', 
   'Ontario', 
-  'kyk_35t'
+  'KJK 45Z'
   );
 
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
-VALUES ('1990-11-11', '1990-11-12', 2, 3), 
-('1990-11-11', '1990-11-12', 2, 2), 
-('1990-11-11', '1990-11-12', 1, 3);
+VALUES ('1990-09-11', '1990-09-26', 2, 3), 
+('1990-01-04', '1990-02-01', 2, 2), 
+('1990-10-01', '1990-10-14', 1, 3);
 
 
 
 
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
-VALUES (3, 1, 1, 5, 'Quite a nice spot!'),
-(2, 2, 2, 4, 'Not too bad!'),
-(3, 3, 3, 3, 'Very mediocre');
+VALUES (3, 1, 1, 5, 'messages'),
+(2, 2, 2, 4, 'messages'),
+(3, 2, 3, 3, 'messages');
