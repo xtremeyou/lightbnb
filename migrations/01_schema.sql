6afc83b24f7c46e4137d2c81d8a28536c7caad0d
xtremeyou <xtremeyou@hotmail.com>
2024-09-01 22:56:46 -0400

    add change spelling of foreign key reservations_id to reservation_id

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -41,7 +41,7 @@ CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
-  reservations_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
   rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
 );
